9e6ee3b514026d3b11029986775306488851f436
Henok-Solomon-T <noksol03@gmail.com>
2023-11-27 13:46:01 -0800

    10 linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * check_cycle- check if the linked list contain a cycle
+ * @list: linked list to check
+ *
+ * Return: 0 if No else 1 if Yes
+ */
+
+int check_cycle(listint_t *list)
+{
+
+}
